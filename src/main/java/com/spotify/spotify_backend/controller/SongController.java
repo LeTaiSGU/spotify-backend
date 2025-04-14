588fe7b4f7f47f8d3248629a5cce13b13e657e63
LeTaiSGU <letaikun11112@gmail.com>
2025-04-14 15:38:34 +0700

    add Song con sr

--- src/main/java/com/spotify/spotify_backend/controller/SongController.java
+++ src/main/java/com/spotify/spotify_backend/controller/SongController.java
@@ -2,6 +2,7 @@ package com.spotify.spotify_backend.controller;
 
 import com.spotify.spotify_backend.dto.ApiResponse;
 import com.spotify.spotify_backend.dto.song.songResponse;
+import com.spotify.spotify_backend.dto.song.songUpdate;
 import com.spotify.spotify_backend.dto.song.songdto;
 // import com.spotify.spotify_backend.mapper.ArtistMappingHelper;
 import com.spotify.spotify_backend.mapper.SongMapper;
@@ -10,6 +11,7 @@ import com.spotify.spotify_backend.model.Song;
 import com.spotify.spotify_backend.service.SongService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,6 +21,8 @@ import java.util.List;
 @RequestMapping("/api/songs")
 @RequiredArgsConstructor
 public class SongController {
+
+    // https://ytmp3.cc/Nnht/ link convert mp3
 
     @Autowired
     private SongService songService;
@@ -32,12 +36,14 @@ public class SongController {
         List<songResponse> songList = songService.getAllSongs().stream()
                 .map(song -> songMapper.toDto(song))
                 .toList();
+        // List<Song> songList = songService.getAllSongs();
         apiResponse.setResult(songList);
         apiResponse.setCode(1000);
         apiResponse.setMessage("List of all songs");
         return apiResponse;
     }
 
+    // upload song
     @PostMapping("/upload")
     public ApiResponse<songResponse> uploadSong(
             @RequestPart("song") songdto song,
@@ -52,7 +58,20 @@ public class SongController {
         return apiResponse;
     }
 
-    @PutMapping("/disable/{songId}")
+    @PutMapping(value = "/update/{songId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ApiResponse<songResponse> editSong(
+            @PathVariable Long songId,
+            @RequestPart("song") songUpdate song,
+            @RequestPart(value = "songFile", required = false) MultipartFile songFile,
+            @RequestPart(value = "imgFile", required = false) MultipartFile imgFile) {
+        ApiResponse<songResponse> apiResponse = new ApiResponse<>();
+        songResponse updatedSong = songService.updateSong(songId, song, songFile, imgFile);
+        apiResponse.setResult(updatedSong);
+        apiResponse.setMessage("Song updated successfully");
+        return apiResponse;
+    }
+
+    @PutMapping("/status/{songId}")
     public ApiResponse<songResponse> disableSong(@PathVariable Long songId) {
         Song song = songService.disableSong(songId);
         songResponse response = songMapper.toDto(song);
